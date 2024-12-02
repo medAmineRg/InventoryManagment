853080a6168ba294302c60071fd8b6813aa05af9
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2024-12-02 10:23:52 +0100

    Implement sign-in functionality, create menu component with logout feature, and refactor authentication handling

--- app/index.jsx
+++ app/index.jsx
@@ -1,51 +1,140 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { useState, useEffect } from "react";
+import useAuthentication from "../hooks/useAuthentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const { signIn, isLoading, isSuccess, error } = useAuthentication();
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = async () => {
+    signIn({ email, password });
+  };
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token || isSuccess) {
+        router.replace("/menu");
+      }
+    };
+    getToken();
+  }, [isSuccess]);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Menu</Text>
-      <Link style={styles.menuItem} href="/signin">
-        <Text style={styles.menuText}>SIGN IN</Text>
-      </Link>
-      <Link style={styles.menuItem} href="/signup">
-        <Text style={styles.menuText}>SIGN UP</Text>
-      </Link>
-      <Link style={styles.menuItem} href="/session">
-        <Text style={styles.menuText}>SESSION</Text>
-      </Link>
-      <Link style={styles.menuItem} href="/setupstock">
-        <Text style={styles.menuText}>SET UP THE STOCK</Text>
+      {error && !isLoading ? (
+        <Text style={styles.errorText}>{error}</Text>
+      ) : null}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+        placeholderTextColor={"#fff"}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        keyboardType="password"
+        secureTextEntry
+        onChangeText={setPassword}
+        placeholderTextColor={"#fff"}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={handleSignIn}
+        disabled={isLoading}>
+        <Text style={styles.btnText}>
+          {isLoading ? "Signing In..." : "Sign In"}
+        </Text>
+      </TouchableOpacity>
+      <Link style={[styles.text, styles.btnText]} href="/signup">
+        Sign Up if you don't have an account
       </Link>
     </View>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#000",
+//   },
+//   title: {
+//     color: "#fff",
+//     fontSize: 24,
+//     marginBottom: 20,
+//     fontWeight: "bold",
+//   },
+//   menuItem: {
+//     width: "80%",
+//     padding: 15,
+//     marginVertical: 10,
+//     backgroundColor: "#db2777",
+//     borderRadius: 5,
+//     alignItems: "center",
+//   },
+//   menuText: {
+//     color: "#fff",
+//     fontSize: 18,
+//     fontWeight: "bold",
+//   },
+// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#000",
+    width: "100%",
   },
-  title: {
-    color: "#fff",
-    fontSize: 24,
-    marginBottom: 20,
-    fontWeight: "bold",
-  },
-  menuItem: {
+  input: {
     width: "80%",
-    padding: 15,
-    marginVertical: 10,
-    backgroundColor: "#db2777",
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 10,
     borderRadius: 5,
-    alignItems: "center",
+    color: "white",
   },
-  menuText: {
+  text: {
+    borderWidth: 1,
+    borderColor: "#db2777",
+    padding: 10,
+    borderRadius: 5,
+    width: "80%",
+    textAlign: "center",
+    marginBottom: 12,
+    backgroundColor: "#db2777",
+  },
+  btn: {
+    width: "80%",
+    borderRadius: 5,
+    backgroundColor: "#2563eb",
+    padding: 10,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  btnText: {
     color: "#fff",
-    fontSize: 18,
     fontWeight: "bold",
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 12,
   },
 });
---
+++ app/menu/index.jsx
@@ -0,0 +1,69 @@
+import { Link, useRouter } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
+import useAuthentication from "../../hooks/useAuthentication";
+
+export default function Menu() {
+  const router = useRouter();
+  const { isSuccess } = useAuthentication();
+
+  // handle log out delete token from local storage
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem("token");
+    router.replace("/");
+  };
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (!token) {
+        router.replace("/");
+      }
+    };
+    getToken();
+  }, [isSuccess]);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu</Text>
+      <Link style={styles.menuItem} href="/session">
+        <Text style={styles.menuText}>SESSION</Text>
+      </Link>
+      <Link style={styles.menuItem} href="/setupstock">
+        <Text style={styles.menuText}>SET UP THE STOCK</Text>
+      </Link>
+      {/* create a link that will signout delete a token from local storage name -> token */}
+      <Link style={styles.menuItem} href="/" onPress={handleLogout}>
+        <Text style={styles.menuText}>SIGN OUT</Text>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  menuItem: {
+    width: "80%",
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: "#db2777",
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  menuText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
